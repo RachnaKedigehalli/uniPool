e5fc6d8ccb72a56af92442adb58524243c466e14
Nandakishore-Menon <nandakishore.menon@iiitb.ac.in>
2023-04-23 21:00:52 +0530

    changed request url

--- src/components/Navbar/ModalContent/RegisterForm.js
+++ src/components/Navbar/ModalContent/RegisterForm.js
@@ -37,7 +37,7 @@ const RegisterForm = () => {
     };
     console.log("body: ", body);
     axios
-      .get("http://api-gateway-svc:9191/users/exists/id/1")
+      .get("/users/exists/id/1")
       .then((res) => {
         console.log("register: ", res);
       })
