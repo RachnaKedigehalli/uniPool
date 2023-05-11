d382ed3db3e8ffef1fb59d4c58a4497823378395
Nandakishore-Menon <nandakishore.menon@iiitb.ac.in>
2023-05-11 23:49:47 +0530

    changed log file

--- src/main/java/com/uniPool/bookingservice/controller/BookingController.java
+++ src/main/java/com/uniPool/bookingservice/controller/BookingController.java
@@ -7,8 +7,8 @@ import com.uniPool.bookingservice.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.validation.Valid;
 
 @RestController
